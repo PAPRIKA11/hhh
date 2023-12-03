8213f6e4a438c82250c8f4fc8799478bdafc14e6
PAPRIKA11 <152810285+PAPRIKA11@users.noreply.github.com>
2023-12-03 21:51:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "FUCK YOU!";
+  question.innerHTML = "FUCK YOU MN LAKHR!";
   gif.src =
     "https://media.giphy.com/media/26BRNKLUezD1NpsOc/giphy.gif";
     yesBtn.style.display = "none";
